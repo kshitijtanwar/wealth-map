bfe56fce688367abc5fe9f88954c9ae98f84e006
Kshitij <kshitij.tanwar2002@gmail.com>
2025-05-09 17:29:49 +0530

    chore: added login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/auth/Login";
 import SignUp from "./pages/auth/SignUp";
-
+import { Toaster } from "sonner";
 const App = () => {
     return (
         <>
+            <Toaster richColors />
             <Router>
                 <Routes>
                     <Route path="/" element={<LoginPage />} />
