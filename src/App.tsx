1970b20607a10ff534d8932455ad7c4907c6427a
Kshitij <kshitij.tanwar2002@gmail.com>
2025-05-09 16:32:52 +0530

    chore: changed the login and sign up

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/auth/Login";
-import CompanyRegistration from "./pages/auth/CompanyRegistration";
-import { useState } from "react";
+import SignUp from "./pages/auth/SignUp";
 
 const App = () => {
-    const [isRegistering, setIsRegistering] = useState(false);
-
     return (
         <>
             <Router>
                 <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            isRegistering ? (
-                                <CompanyRegistration
-                                    onRegister={() => {}}
-                                    onCancel={() => setIsRegistering(false)}
-                                />
-                            ) : (
-                                <LoginPage
-                                    onLogin={() => {}}
-                                    onForgotPassword={() => {}}
-                                    onRegister={() => setIsRegistering(true)}
-                                />
-                            )
-                        }
-                    />
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignUp />} />
                 </Routes>
             </Router>
         </>
