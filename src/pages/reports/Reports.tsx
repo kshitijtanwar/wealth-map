9d42bd836f4fdba039ede5bdf87e100aa90d1d38
Kshitij <kshitij.tanwar2002@gmail.com>
2025-05-11 03:46:48 +0530

    feat: add Reports page and update routing in AppRoutes and AppSidebar

---
+++ src/pages/reports/Reports.tsx
@@ -0,0 +1,10 @@
+const Reports = () => {
+    return (
+        <section>
+            <h1 className="text-center font-bold text-2xl my-6">
+                Reports Coming soon!
+            </h1>
+        </section>
+    );
+};
+export default Reports;
