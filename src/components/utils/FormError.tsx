0b5ea9447b08535700b12f8067f7b185b1d6eef2
Kshitij <kshitij.tanwar2002@gmail.com>
2025-05-12 22:26:05 +0530

    feat: implement onboarding form with validation and error handling

--- src/components/utils/FormError.tsx
+++ src/components/utils/FormError.tsx
@@ -1,4 +1,4 @@
 const FormError = ({ text }: { text?: string }) => {
-    return <p className="text-red-500 text-xs text-right">{text}</p>;
+    return <p className="text-red-500 text-xs text-right p-0 m-0">{text}</p>;
 };
 export default FormError;
